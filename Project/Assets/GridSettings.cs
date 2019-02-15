9999465627b87e822a5ba16365aae19f34cf128a
Kacper Obara <kacper97pl@tlen.pl>
2019-02-15 12:00:00 +0100

    Initial commit

---
+++ Project/Assets/GridSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GridSettings : ScriptableObject
+{
+    public int WorldWidth;
+    public int WorldHeight;
+
+    public Hex hexPrefab;
+}
---
+++ Project/Assets/HexGrid.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    public GridSettings settings;
+
+    private Hex[,] hexes;
+
+    private void Awake()
+    {
+        hexes = new Hex[settings.WorldWidth, settings.WorldHeight];
+        GenerateGrid();
+    }
+
+    private void GenerateGrid()
+    {
+        for(int x = 0; x < settings.WorldWidth; ++x)
+        {
+            for (int z = 0; z < settings.WorldHeight; ++z)
+            {
+                InstantiateHex(x, z);
+            }
+        }
+    }
+
+    private void InstantiateHex(int x, int z)
+    {
+        //Vector3 pos = Vector3.zero;
+        //pos.x = x * Hex.OuterRadius;
+        //pos.z = z * Hex.OuterRadius;
+
+        hexes[x, z] = Instantiate(settings.hexPrefab, CalcPos(x, z), Quaternion.identity);
+        hexes[x, z].transform.parent = transform;
+    }
+
+    private Vector3 CalcPos(int x, int z)
+    {
+        Vector3 pos = Vector3.zero;
+        pos.x = x * Hex.OuterRadius;
+        pos.z = z * Hex.OuterRadius;
+        pos.x = (x + z * 0.5f - z / 2) * (Hex.InnerRadius * 2f);
+        return pos;
+        /*
+        float offset = 0f;
+
+        if (z % 2 != 0)
+            offset = Hex.InnerRadius / 2;
+
+        return new Vector3(x * Hex.InnerRadius + offset, 0, z * Hex.OuterRadius);
+        */
+    }
+}
